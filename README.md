29dec0a000bffee153bf59b6e910582da1ae5a29
J Loo <junzhongloo@gmail.com>
2021-05-06 19:37:20 +1200

    added AtIndex functions AND updated readme

--- README.md
+++ README.md
@@ -5,7 +5,15 @@
 - remove() - removes first node form the list and returns node. If list is empty, returns None
 - printList() - prints list 
 - getSize() - prints total number of Nodes in list
-- getLastNode - prints val of last Node.
+- getLastNode() - prints val of last Node.
 
 ## Double LinkedList
-- 
+- addNodeLast(val) - add Node(val) to the end of the list
+- addNodeStart(val) - add Node(val) to start of list
+- addNodeAtIndex(index, val) - add Node(val) at list[index]
+- removeNodeAtIndex(index) - remove Node at list[index]
+- removeFirstNode() - remove first Node
+- removeLastNode() - remove last Node
+- printList() - prints list 
+- getSize - prints total number of Nodes in list
+- backwards_traverse() - prints list backwards
--- double_linked_list.py
+++ double_linked_list.py
@@ -9,21 +9,20 @@ class Double_LL():
         self.head = head
         self.tail = self.head
 
+
     def addNodeLast(self,val):
         node = Node(val)
-        if self.head == None:
+        if self.tail == None:
             # if no node in list
             node.right = None
             self.head = node
             self.tail = node
         else:
-            temp = self.head
-            while temp.right is not None:
-                temp = temp.right
-            temp.right = node
-            node.left = temp
+            self.tail.right = node
+            node.left = self.tail
             self.tail = node
-    
+
+
     def addNodeStart(self,val):
         node = Node(val)
         if self.head == None:
@@ -34,6 +33,7 @@ class Double_LL():
             node.right = self.head
             self.head.left = node
             self.head = node 
+
     
     def addNodeAtIndex(self, index, val):
         if index >= self._size():
@@ -48,8 +48,39 @@ class Double_LL():
         node.right = temp
         temp.left = node
 
-            
-    
+
+    def removeNodeAtIndex(self, index):
+        if index == 0:
+            self.removeFirstNode()
+
+        if index >= self._size():
+            raise IndexError("index out of range")
+
+        temp = self.head
+        while index > 0:
+            temp = temp.right
+            index -= 1
+        temp.left.right = temp.right
+        temp.right.left = temp.left
+
+
+    def removeFirstNode(self):
+        if self.head == None:
+            print('List is empty')
+            return -1
+        self.head.right.left = None
+        self.head = self.head.right
+        return 1
+
+    def removeLastNode(self):
+        if self.tail == None:
+            print('List is empty')
+            return -1
+        self.tail.left.right = None
+        self.tail = self.tail.left
+        return 1
+
+
     def printList(self):
         temp = self.head
         print('Head ->', end = ' ')
@@ -58,6 +89,7 @@ class Double_LL():
             temp = temp.right
         print('None')
 
+
     def getSize(self):
         count = 0
         temp = self.head
@@ -65,6 +97,7 @@ class Double_LL():
             count += 1
             temp = temp.right
         print('Total node in list:', count)
+
     
     def _size(self):
         count = 0
@@ -73,6 +106,7 @@ class Double_LL():
             count += 1
             temp = temp.right
         return count
+
 
     def backward_traverse(self):
         temp = self.tail
@@ -94,3 +128,10 @@ if __name__ == "__main__":
     newDLL.addNodeAtIndex(3,88)
     newDLL.printList()
     newDLL.backward_traverse()
+    newDLL.removeFirstNode()
+    newDLL.printList()
+    newDLL.removeLastNode()
+    newDLL.printList()
+    newDLL.backward_traverse()
+    newDLL.removeNodeAtIndex(1)
+    newDLL.printList()
